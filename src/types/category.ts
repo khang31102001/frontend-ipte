8b2e4f3e1c3fc49c69fd045ff46d3e6872286f5c
khang31102001 <leminhkhang.ltp@gmail.com>
2025-10-07 15:55:31 +0700

    update okey

---
+++ src/types/category.ts
@@ -0,0 +1,39 @@
+
+export interface Category {
+    id: number
+    icon?: string,
+    name: string,
+    slug: string,
+    description?: string,
+    parent_id: number | null,
+    category_type: string,
+    // --- CÁC TRƯỜNG THUỘC TÍNH SEO CHUẨN --- //
+    meta_title?: string,
+    meta_description?: string,
+    h1_heading?: string,
+    seo_content_top?: string,
+    seo_content_bottom?: string,
+    canonical_url?: string,
+    noindex?: boolean
+}
+
+export interface Categories {
+    id: number
+    icon?: string,
+    name: string,
+    slug: string,
+    description?: string,
+    parent_id: number | null,
+    children: Categories[] ,
+    level: number,
+    category_type: string,
+    meta_title?: string,
+    meta_description?: string,
+    h1_heading?: string,
+    seo_content_top?: string,
+    seo_content_bottom?: string,
+    canonical_url?: string,
+    noindex?: boolean
+}
+
+
